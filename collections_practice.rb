4e4ba0a7716a2a381a73093183692b35e5181791
Haley Grassi <teacherhaleymarie@gmail.com>
2020-08-30 16:28:32 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -2,6 +2,12 @@
 def sort_array_asc (array)
   array.sort
 end
+#Q 2
 def sort_array_desc (array)
   array.sort {|x,y| y <=> x }
+end
+# Q 3
+def swap_elements (array)
+  array[1], array[2] = array[2], array[1]
+  array
 end
